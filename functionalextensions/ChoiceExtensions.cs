ae1bd939155aa76aec51c9c915a7f89bae11ac86
Leif Battermann <leif.battermann@webrunners.de>
2015-01-01 23:33:18 +0100

    option and choice: redundant map extension removed

--- functionalextensions/ChoiceExtensions.cs
+++ functionalextensions/ChoiceExtensions.cs
@@ -4,23 +4,6 @@ namespace FunctionalExtensions
 {
     public static class ChoiceExtensions
     {
-        public static Choice<TResult, T2> Map<T1, T2, TResult>(this Choice<T1, T2> source, Func<T1, TResult> selector)
-        {
-            T1 value1;
-            if (source.MatchChoice1Of2(out value1))
-            {
-                return Choice.NewChoice1Of2<TResult, T2>(selector(value1));
-            }
-
-            T2 value2;
-            if (source.MatchChoice2Of2(out value2))
-            {
-                return Choice.NewChoice2Of2<TResult, T2>(value2);
-            }
-
-            throw new InvalidOperationException("source (:Choice) has no value.");
-        }
-
         public static Choice<TResult, T2> Bind<T1, T2, TResult>(this Choice<T1, T2> source, Func<T1, Choice<TResult, T2>> selector)
         {
             T1 value1;
@@ -40,12 +23,24 @@ namespace FunctionalExtensions
 
         public static Choice<TResult, T2> Select<T1, T2, TResult>(this Choice<T1, T2> source, Func<T1, TResult> selector)
         {
-            return source.Map(selector);
+            T1 value1;
+            if (source.MatchChoice1Of2(out value1))
+            {
+                return Choice.NewChoice1Of2<TResult, T2>(selector(value1));
+            }
+
+            T2 value2;
+            if (source.MatchChoice2Of2(out value2))
+            {
+                return Choice.NewChoice2Of2<TResult, T2>(value2);
+            }
+
+            throw new InvalidOperationException("source (:Choice) has no value.");
         }
 
         public static Choice<TResult, T2> SelectMany<TSource, TValue, T2, TResult>(this Choice<TSource, T2> source, Func<TSource, Choice<TValue, T2>> valueSelector, Func<TSource, TValue, TResult> resultSelector)
         {
-            return source.Bind(s => valueSelector(s).Map(v => resultSelector(s, v)));
+            return source.Bind(s => valueSelector(s).Select(v => resultSelector(s, v)));
         }
 
         public static Choice<T, T2> ToChoice<T, T2>(this Option<T> source, T2 value)
--- functionalextensions/OptionExtensions.cs
+++ functionalextensions/OptionExtensions.cs
@@ -4,12 +4,6 @@ namespace FunctionalExtensions
 {
     public static class OptionExtensions
     {
-        public static Option<TResult> Map<T, TResult>(this Option<T> source, Func<T, TResult> selector)
-        {
-            T value;
-            return source.MatchSome(out value) ? Option.Some(selector(value)) : Option.None<TResult>();
-        }
-
         public static Option<TResult> Bind<T, TResult>(this Option<T> source, Func<T, Option<TResult>> selector)
         {
             T value;
@@ -18,12 +12,13 @@ namespace FunctionalExtensions
 
         public static Option<TResult> Select<T, TResult>(this Option<T> source, Func<T, TResult> selector)
         {
-            return source.Map(selector);
+            T value;
+            return source.MatchSome(out value) ? Option.Some(selector(value)) : Option.None<TResult>();
         }
 
         public static Option<TResult> SelectMany<TSource, TValue, TResult>(this Option<TSource> source, Func<TSource, Option<TValue>> valueSelector, Func<TSource, TValue, TResult> resultSelector)
         {
-            return source.Bind(s => valueSelector(s).Map(v => resultSelector(s, v)));
+            return source.Bind(s => valueSelector(s).Select(v => resultSelector(s, v)));
         }
 
         public static Option<T> ToOption<T>(this T value)
