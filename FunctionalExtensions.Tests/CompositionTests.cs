316ac2bccc3d5ff2323b48c467e55635f8c9fa38
Leif Battermann <leif.battermann@arcor.de>
2015-05-21 23:22:02 +0200

    trying out some composition stuff

---
+++ FunctionalExtensions.Tests/CompositionTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FunctionalExtensions.Currying;
+using FunctionalExtensions.Lambda;
+using NFluent;
+using NUnit.Framework;
+
+namespace FunctionalExtensions.Tests
+{
+    [TestFixture]
+    public class CompositionTests
+    {
+        [Test]
+        public void Compose_Test()
+        {
+            var f = new Func<string, IEnumerable<string>>(s => s.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries));
+            var g = new Func<string, string>(s => s.ToUpper());
+
+            var h = f.Compose(g);
+
+            var result = h("foo bar");
+
+            Check.That(result).ContainsExactly("FOO", "BAR");
+
+            var inc = new Func<int, int>(i => i + 1);
+            var compose = inc.Compose(inc);
+            Check.That(compose(1)).IsEqualTo(3);
+        }
+
+        [Test]
+        public void Compose_2_Test()
+        {
+            var multiply = new Func<int, int, int>((x, y) => x*y);
+            var inc = new Func<int, int>(i => i + 1);
+
+            var compose = multiply.Compose(inc);
+
+            var result = compose(7, 8);
+            Check.That(result).IsEqualTo(64);
+        }
+
+        [Test]
+        public void Compose_3_Test()
+        {
+            var multiply3 = new Func<int, int, int, int>((x, y, z) => x * y * z);
+            var inc = new Func<int, int>(i => i + 1);
+
+            var compose = multiply3.Compose(inc);
+
+            var result = compose(1, 2, 3);
+            Check.That(result).IsEqualTo(12);
+        }
+
+        [Test]
+        public void Test()
+        {
+            var inc = new Func<int, int>(i => i + 1);
+            var multiply = new Func<int, int, int>((x, y) => x*y);
+
+            var c = Fun.Create((int i) => multiply.Curry()(i));
+
+            var compose = inc.Compose(multiply);
+
+
+        }
+    }
+}
---
+++ FunctionalExtensions/Composition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FunctionalExtensions
+{
+    public static class CompositionExtensions
+    {
+        public static Func<T1, T3> Compose<T1, T2, T3>(this Func<T2, T3> f, Func<T1, T2> g)
+        {
+            return x => f(g(x));
+        }
+
+        public static Func<T1, T2, T4> Compose<T1, T2, T3, T4>(this Func<T3, T4> f, Func<T1, T2, T3> g)
+        {
+            throw new NotImplementedException();
+            //return (x1, x2) => f(g(x));
+        }
+
+        public static Func<T1, T3, T4> Compose<T1, T2, T3, T4>(this Func<T2, T3, T4> f, Func<T1, T2> g)
+        {
+            return (x1, x2) => f(g(x1), x2);
+        }
+
+        public static Func<T1, T3, T4, T5> Compose<T1, T2, T3, T4, T5>(this Func<T2, T3, T4, T5> f, Func<T1, T2> g)
+        {
+            return (x1, x2, x3) => f(g(x1), x2, x3);
+        }
+    }
+}
