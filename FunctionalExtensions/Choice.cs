08bb310965071c650f6a46bd7f9a9dd3c8d9effe
Christoph von Krüchten <christoph@vonkruechten.de>
2017-09-12 14:21:08 +0200

    Minor Choice cleanup

--- FunctionalExtensions/Choice.cs
+++ FunctionalExtensions/Choice.cs
@@ -4,9 +4,11 @@ namespace FunctionalExtensions
 {
     public struct Choice<T1, T2>
     {
-        private readonly ChoiceType _tag;
         private readonly T1 _choice1Of2;
         private readonly T2 _choice2Of2;
+
+        internal bool IsChoice1Of2 => Tag == ChoiceType.Choice1Of2;
+        internal bool IsChoice2Of2 => Tag == ChoiceType.Choice2Of2;
 
         internal Choice(T1 choice1Of2)
         {
@@ -14,7 +16,7 @@ namespace FunctionalExtensions
                 throw new ArgumentNullException(nameof(choice1Of2));
             _choice1Of2 = choice1Of2;
             _choice2Of2 = default(T2);
-            _tag = ChoiceType.Choice1Of2;
+            Tag = ChoiceType.Choice1Of2;
         }
 
         internal Choice(T2 choice2Of2)
@@ -23,27 +25,31 @@ namespace FunctionalExtensions
                 throw new ArgumentNullException(nameof(choice2Of2));
             _choice2Of2 = choice2Of2;
             _choice1Of2 = default(T1);
-            _tag = ChoiceType.Choice2Of2;
+            Tag = ChoiceType.Choice2Of2;
         }
 
-        public ChoiceType Tag { get { return _tag; } }
+        public ChoiceType Tag { get; }
 
         internal bool MatchChoice1Of2(out T1 value)
         {
-            value = Tag == ChoiceType.Choice1Of2 ? _choice1Of2 : default(T1);
-            return Tag == ChoiceType.Choice1Of2;
+            value = IsChoice1Of2 
+                ? _choice1Of2 
+                : default(T1);
+            return IsChoice1Of2;
         }
 
         internal bool MatchChoice2Of2(out T2 value)
         {
-            value = Tag == ChoiceType.Choice2Of2 ? _choice2Of2 : default(T2);
-            return Tag == ChoiceType.Choice2Of2;
+            value = IsChoice2Of2 
+                ? _choice2Of2 
+                : default(T2);
+            return IsChoice2Of2;
         }
 
         [Obsolete("Match is deprecated, please use overload of Match: TResult Match<TResult>(Func<T1, TResult> onChoice1Of2, Func<T2, TResult> onChoice2Of2) instead.")]
         public void Match(Action<T1> onChoice1Of2, Action<T2> onChoice2Of2)
         {
-            if (Tag == ChoiceType.Choice1Of2)
+            if (IsChoice1Of2)
                 onChoice1Of2(_choice1Of2);
             else
                 onChoice2Of2(_choice2Of2);
@@ -51,15 +57,17 @@ namespace FunctionalExtensions
 
         public TResult Match<TResult>(Func<T1, TResult> onChoice1Of2, Func<T2, TResult> onChoice2Of2)
         {
-            return Tag == ChoiceType.Choice1Of2 
+            return IsChoice1Of2 
                 ? onChoice1Of2(_choice1Of2) 
                 : onChoice2Of2(_choice2Of2);
         }
 
         public override string ToString()
         {
-            var t1T2 = string.Format("<{0}, {1}>", typeof (T1).Name, typeof (T2).Name);
-            return Tag == ChoiceType.Choice1Of2 ? String.Format("Choice1Of2{1}({0})", _choice1Of2, t1T2) : String.Format("Choice2Of2{1}({0})", _choice2Of2, t1T2);
+            var t1T2 = $"<{typeof(T1).Name}, {typeof(T2).Name}>";
+            return IsChoice1Of2 
+                ? $"Choice1Of2{t1T2}({_choice1Of2})"
+                : $"Choice2Of2{t1T2}({_choice2Of2})";
         }
     }
 
